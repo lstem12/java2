0a46edf12a2948206a914f58527efddc3649bdb0
lstem12 <lstem1217@gmail.com>
2020-06-23 16:12:45 +0900

    로또 번호 뽑기및 중복제거

--- src/s0623/Execute4.java
+++ src/s0623/Execute4.java
@@ -1,13 +1,24 @@
 package s0623;
 
+import java.util.Random;
+
 public class Execute4 {
 	public static void main(String[] args) {
-		Cat[] cats = new Cat[2];
+		Cat[] cats = new Cat[20];
+		
+		Random ran = new Random();
+		
 		for(int i=0;i<cats.length;i++) {
+			int num = ran.nextInt(40)+1;
 			cats[i] = new Cat();
+			cats[i].setName("고양이" + i);
+			cats[i].setAge(num);
 		}
-		cats[0].setName("동동이");
-		cats[0].setAge(7);
-		System.out.println(cats[0]);
+		/*for(int i=0;i<cats.length;i++) {
+		System.out.println(cats[i]+"\r");
+		}*/
+		for(Cat cat:cats) {
+			System.out.println(cat);
+		}
 	}
 }
