8405f365f2f36de2db2c88ca76d4c85daf469f8c
lstem12 <lstem1217@gmail.com>
2020-06-23 10:52:20 +0900

    123

---
+++ src/s0623/StaticTest.java
@@ -0,0 +1,15 @@
+package s0623;
+
+public class StaticTest {
+	static String str;
+	int num;
+	public static void main(String[] args) {		
+		System.out.println(str);
+		StaticTest st = new StaticTest();
+		System.out.println(st.num);
+		st.num = 10;
+		
+		StaticTest st2 = new StaticTest();
+		System.out.println(st2.num);
+	}
+}
