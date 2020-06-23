d3567166bfe96f59881cfcff25e9828ef655d695
lstem12 <lstem1217@gmail.com>
2020-06-23 09:53:28 +0900

    6.23 9:50 git test

---
+++ src/s0623/Computer.java
@@ -0,0 +1,22 @@
+package s0623;
+
+public class Computer {
+	String cpu;
+	int ram;
+	String board;
+	Computer(String cpu, int ram, String board){
+		this.cpu = cpu;
+		this.ram = ram;
+		this.board = board;
+	}
+	
+	void upgrade(String cpu) {
+		this.cpu = cpu;
+	}
+	
+	void printInfo() {
+		System.out.println("CPU :" + this.cpu);
+		System.out.println("RAM :" + this.ram + "G");
+		System.out.println("Main Board :" + this.board);
+	}
+}
---
+++ src/s0623/Execute.java
@@ -0,0 +1,9 @@
+package s0623;
+
+public class Execute {
+	public static void main(String[] args) {
+		Computer c;
+		c = new Computer("intel i7 9700",16,"Gigabyte B370");
+		c.printInfo();
+	}
+}
